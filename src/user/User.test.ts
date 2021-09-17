12d19440247410dbe187ffdff8500ad7a71b914b
ptlm500 <peter_mills@hotmail.co.uk>
2021-09-17 19:27:05 +0100

    Remove mocked discord

--- src/user/User.test.ts
+++ src/user/User.test.ts
@@ -1,86 +1,24 @@
-import { mocked } from 'ts-jest/utils';
-import { Role as DiscordRole } from 'discord.js';
+import { Collection } from 'discord.js';
 import User from './User';
-import { AdminRole } from '../entities';
-import MockDiscord from '../../test/MockDiscord';
-import { getAdminRoles} from "../services/serverManagementService";
 
 jest.mock("../services/serverManagementService");
 
-const mockedGetAdminRoles = mocked(getAdminRoles);
-
-const discord = new MockDiscord();
-
 describe('canAdministerServer', () => {
   it('returns true when permissions contains the administrator permission', () => {
-    const user = new User(["ADMINISTRATOR"], []);
+    const user = new User(["ADMINISTRATOR"], new Collection([]));
 
     expect(user.canAdministerServer()).toBeTruthy();
   });
 
   it('returns false when permissions contains no permissions', () => {
-    const user = new User([], []);
+    const user = new User([], new Collection([]));
 
     expect(user.canAdministerServer()).toBeFalsy();
   });
 
   it('returns false when permissions doesn\'t contain the adminstrator permission', () => {
-    const user = new User(["MANAGE_ROLES", "MANAGE_NICKNAMES"], []);
+    const user = new User(["MANAGE_ROLES", "MANAGE_NICKNAMES"], new Collection([]));
 
     expect(user.canAdministerServer()).toBeFalsy();
   });
 });
-
-describe('canAdministerRoles', () => {
-  const serverId = 'test_server';
-  const adminDiscordRole = getDiscordRole('admin');
-  const nonAdminDiscordRole = getDiscordRole('user');
-  const adminRole = new AdminRole();
-  adminRole.discordId = adminDiscordRole.id;
-
-  mockedGetAdminRoles.mockImplementation(() => Promise.resolve([adminRole]));
-
-  it('returns true when permissions contain the administrator permission, and user has an admin role', async () => {
-    const user = new User(["ADMINISTRATOR"], [adminDiscordRole]);
-
-    const result = await user.canAdministerRoles(serverId);
-    expect(result).toBeTruthy();
-  });
-
-  it('returns true when permissions contain the administrator permission, and user has no admin role', async () => {
-    const user = new User(["ADMINISTRATOR"], []);
-
-    const result = await user.canAdministerRoles(serverId);
-    expect(result).toBeTruthy();
-  });
-
-  it('returns true when permissions don\'t contain the administrator permission, and user has an admin role', async () => {
-    const user = new User(["ADD_REACTIONS"], [adminDiscordRole]);
-
-    const result = await user.canAdministerRoles(serverId);
-    expect(result).toBeTruthy();
-  });
-
-  it('returns false when permissions don\'t contain the administrator permission, and user has no admin role', async () => {
-    const user = new User(["ADD_REACTIONS"], [nonAdminDiscordRole]);
-
-    const result = await user.canAdministerRoles(serverId);
-    expect(result).toBeFalsy();
-  });
-
-  it('returns false when permissions don\'t contain the administrator permission, and user has no admin role', async () => {
-    const user = new User(["ADD_REACTIONS"], []);
-
-    const result = await user.canAdministerRoles(serverId);
-    expect(result).toBeFalsy();
-  });
-});
-
-afterAll(() => {
-  discord.cleanup();
-  jest.restoreAllMocks();
-});
-
-function getDiscordRole(id: string): DiscordRole {
-  return new DiscordRole(discord.getClient(), { id }, discord.getGuild());
-}
