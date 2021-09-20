48b4847caa02d3b5a959e960e0325605ed575e50
ptlm500 <peter_mills@hotmail.co.uk>
2021-09-20 20:12:58 +0100

    Use URL for ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,12 @@ const { Role, AdminRole, Server } = require("./dist/entities");
 
 module.exports = {
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_NAME,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
   entities: [ Role, AdminRole, Server ],
   synchronize: true,
   logging: true
