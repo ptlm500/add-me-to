acc6df682f9620020aae10de07cfbc5bce190242
ptlm500 <peter_mills@hotmail.co.uk>
2022-10-10 21:19:17 +0100

    set SSL from env vars

--- ormconfig.js
+++ ormconfig.js
@@ -3,18 +3,8 @@ const { Role, AdminRole, Server } = require("./dist/entities");
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // database: process.env.DB_NAME,
-  // username: process.env.DB_USERNAME,
-  // password: process.env.DB_PASSWORD,
   entities: [ Role, AdminRole, Server ],
-  synchronize: true,
+  synchronize: false,
   logging: true,
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
+  ssl: process.env.ENABLE_SSL === 'true' ? true : false,
 };
